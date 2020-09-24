6d5cc260edef9a35601d310474795ad6733f6592
qianmua <49187025+qianmua@users.noreply.github.com>
2020-09-24 23:37:09 +0800

    购销合同附件模块 / 组别

--- service/service_factory/src/main/java/com/oscars/factory/controller/SysCodeBController.java
+++ service/service_factory/src/main/java/com/oscars/factory/controller/SysCodeBController.java
@@ -1,6 +1,15 @@
 package com.oscars.factory.controller;
 
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.oscars.common.R;
+import com.oscars.factory.entity.SysCodeB;
+import com.oscars.factory.service.SysCodeBService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -14,8 +23,20 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2020-09-24
  */
 @RestController
-@RequestMapping("/factory/sys-code-b")
+@RequestMapping("/service/factory/sys-code-b")
+@Api("分类控制器")
 public class SysCodeBController {
+
+    @Autowired
+    private SysCodeBService sysCodeBService;
+
+    @ApiOperation("查询所有分类")
+    @PostMapping("/queryAll/{current}/{limit}")
+    public R queryAll(@PathVariable long current , @PathVariable long limit){
+
+        return R.ok()
+                .put("rows" ,sysCodeBService.page(new Page<>(current, limit) , null).getRecords());
+    }
 
 }
 
